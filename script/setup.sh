6c785e60a7133ba40da94c6651a1bd2606014c5f
Yash Nayak <yugn27@gmail.com>
2018-09-13 22:22:33 +0530

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -13,12 +13,7 @@ echo "Welcome to Kioski Installer"
 echo "Steps include:"
 echo "Update package index files (apt-get update)"
 echo "Install prerequisite software"
-echo "Install RGB display driver software and examples"
-echo "Configure boot options"
 echo "Run time ~15 minutes. Some options require reboot."
-echo "EXISTING INSTALLATION, IF ANY, WILL BE OVERWRITTEN."
-
-
 
 
 
@@ -36,21 +31,21 @@ echo "Downloading prerequisites"
 
 sudo apt-get install -y apache2
 sudo echo "ServerName localhost" >> /etc/apache2/httpd.conf
-echo "Apache2 Done"
+echo "Done"
 
 
 #PHP
 sudo apt-get install -y php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl php5-gd php5-sqlite
-echo "Php Done"
+echo "Done"
 
 #MySQL
 sudo apt-get install -y mysql-server mysql-client php5-mysql
-echo "MySql Done"
+echo "Done"
 
 
 #Unclutter
 sudo apt-get install unclutter
-echo "unclutter Done"
+echo "Done"
 
 
 echo "Installation done"
